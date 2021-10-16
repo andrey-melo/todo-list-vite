8e9074440a785eeedbc0a68e07eda93d559a40f2
Andrey Melo <andreyferreirams@gmail.com>
2021-10-16 15:47:32 -0400

    Commit Inicial

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,73 @@
 <template>
-  <h1>{{ titulo }}</h1>
-  <br />
-  <label for="descricao">Descrição: </label>
-  <input
-    v-model="tarefa.descricao"
-    id="descricao"
-    type="text"
-    placeholder="Digite a descrição da tarefa"
-  />
-  <br />
-  <label for="data">Data: </label>
-  <input v-model="tarefa.data" id="data" type="date" />
-  <br />
-  <button @click="salvar">Salvar</button>
-  <br />
+  <h1 class="text-6xl text-center mt-20 mb-20">{{ titulo }}</h1>
 
-<TabelaTarefas :tarefas="tarefas"></TabelaTarefas>
+  <div class="grid grid-cols-12 lg:grid-cols-3">
+    <div
+      class="
+        space-y-4
+        border-2 border-gray-300
+        rounded-sm
+        p-8
+        col-start-2 col-span-10
+        lg:col-start-2 lg:col-span-1
+      "
+    >
+      <div>
+        <label class="block" for="descricao">Descrição: </label>
+        <input
+          class="
+            border-2 border-gray-300
+            outline-none
+            rounded-sm
+            px-4
+            py-2
+            focus:border-blue-600
+            w-full
+          "
+          v-model="tarefa.descricao"
+          id="descricao"
+          type="text"
+          placeholder="Digite a descrição da tarefa"
+        />
+      </div>
+      <div>
+        <label class="block" for="data">Data: </label>
+        <input
+          class="
+            border-2 border-gray-300
+            outline-none
+            rounded-sm
+            px-4
+            py-2
+            focus:border-blue-600
+            w-full
+          "
+          v-model="tarefa.data"
+          id="data"
+          type="date"
+        />
+      </div>
+      <app-btn class="mx-auto" @click="salvar">Salvar</app-btn>
+    </div>
+  </div>
+
+  <div class="grid grid-cols-12 lg:grid-cols-3 mt-10">
+    <div class="border-2 border-gray-300 col-span-10 lg:col-span-1 col-start-2 lg:col-start-2">
+      <TabelaTarefas :tarefas="tarefas"></TabelaTarefas>
+    </div>
+  </div>
 </template>
 
 
 
 <script>
-import TabelaTarefas from './components/TabelaTarefa.vue'
+import TabelaTarefas from "./components/TabelaTarefas.vue";
+import AppBtn from "./components/AppBtn.vue";
 
 export default {
-  components:{
-    TabelaTarefas
+  components: {
+    TabelaTarefas,
+    AppBtn,
   },
   data() {
     return {
--- src/components/TabelaTarefas.vue
+++ src/components/TabelaTarefas.vue
@@ -1,37 +1,43 @@
 <template>
-    <table border="1">
-    <thead>
-      <th>Descrição</th>
-      <th>Data</th>
-      <th>Concluída</th>
-    </thead>
+  <table class="w-full text-md bd-white rounded mb-4">
     <tbody>
+      <tr class="border-b">
+        <th class="text-left p-3 px-5">Descrição</th>
+        <th class="text-left p-3 px-5">Data</th>
+        <th class="text-left p-3 px-5">Concluída</th>
+        <th></th>
+      </tr>
       <tr
-        v-for="item in tarefas"
+        v-for="(item, i) in tarefas"
         :key="item.descricao"
-        :class="{ riscado: item.concluida }"
+        :class="[
+          { riscado: item.concluida },
+          { 'bg-gray-100': i % 2 == 0 },
+          'border-b hover:bg-gray-300',
+        ]"
       >
-        <td>{{ item.descricao }}</td>
-        <td>{{ formatarData(item.data) }}</td>
-        <td><input v-model="item.concluida" type="checkbox" /></td>
+        <td class="p-3 px-5">{{ item.descricao }}</td>
+        <td class="p-3 px-5">{{ formatarData(item.data) }}</td>
+        <td class="p-3 px-5">
+          <input v-model="item.concluida" type="checkbox" />
+        </td>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 
 export default {
-    props: ["tarefas"],
-        methods: {
-            formatarData(data){
-                return moment(data).format("DD/MM/yyyy")
-            }
-        }
-    }
+  props: ["tarefas"],
+  methods: {
+    formatarData(data) {
+      return moment(data).format("DD/MM/yyyy");
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
